8da1de72e9c61a9050c839f06c675e038faa13c1
Ravneet Kaur <ravnett10@gmail.com>
2022-04-24 23:41:24 +1000

    feature #339719: FE error handling

--- InstitutionalPricingTool/ClientApp/src/components/FacilitiesTable.js
+++ InstitutionalPricingTool/ClientApp/src/components/FacilitiesTable.js
@@ -2,65 +2,109 @@ import React, { useState, useEffect } from "react";
 import { getFacilities } from "./interface";
 import "react-table-v6/react-table.css";
 import styles from "./Facilities.module.css";
-import { DropdownMenu, DropdownItem, UncontrolledDropdown , DropdownToggle} from "reactstrap";
+import {
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+} from "reactstrap";
 
 function FacilitiesTable(props) {
   const [facilitiesList, setFacilities] = useState([]);
   const [selectedFacility, setSelectedFacility] = useState(null);
   const [dropDownValue, setDropDownValue] = useState("Select Option");
+  const [errorMessage, seterrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getFacilities(props.proposalId).then((result) => {
-      setFacilities(result);
-      if(result && result.length>0){
-        setSelectedFacility(result[0]);
-        setDropDownValue(result[0].facilityName);
-      }
-    });
+    setIsLoading(true);
+    getFacilities(props.proposalId)
+      .then((result) => {
+        setFacilities(result);
+        setIsLoading(true);
+        if (result && result.length > 0) {
+          setSelectedFacility(result[0]);
+          setDropDownValue(result[0].facilityName);
+        }
+      })
+      .catch((x) => {
+        seterrorMessage(x);
+        setIsLoading(true);
+      });
   }, [props.proposalId]);
-const onMenuChange=(selectedFacility)=>{
+  const onMenuChange = (selectedFacility) => {
     setDropDownValue(selectedFacility.facilityName);
     setSelectedFacility(selectedFacility);
-}
+  };
   return (
     <>
-      {facilitiesList && facilitiesList.length > 0 ? (
+      {errorMessage != null && <div>Some Error Occurred</div>}
+      {isLoading && <div>Loading in progress ...</div>}
+      {!isLoading && facilitiesList && facilitiesList.length > 0 ? (
         <table>
-            <tr>
-          <th>
+          <tr>
+            <th>
               Facility
-            <UncontrolledDropdown className={styles.dropdownParent} >
-              <DropdownToggle caret size="sm" className={styles.dropdownToggle}>{dropDownValue}</DropdownToggle>
-              <DropdownMenu> {facilitiesList.map((option, index) => <DropdownItem key={option.id} 
-              onClick={()=>onMenuChange(option)}>{option.facilityName}</DropdownItem>)}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </th>
+              <UncontrolledDropdown className={styles.dropdownParent}>
+                <DropdownToggle
+                  caret
+                  size="sm"
+                  className={styles.dropdownToggle}
+                >
+                  {dropDownValue}
+                </DropdownToggle>
+                <DropdownMenu>
+                  {" "}
+                  {facilitiesList.map((option, index) => (
+                    <DropdownItem
+                      key={option.id}
+                      onClick={() => onMenuChange(option)}
+                    >
+                      {option.facilityName}
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </th>
           </tr>
           <tr className={styles.flexRowDiv}>
             <td className={styles.flexColumnDiv}>
-                <span>Booking Country</span>
-                <span>{selectedFacility && selectedFacility.bookingCountry}</span>
+              <span>Booking Country</span>
+              <span>{selectedFacility && selectedFacility.bookingCountry}</span>
             </td>
-            <td className={styles.flexColumnDiv}>Currency 
-            <span>{selectedFacility && selectedFacility.currency}</span>
+            <td className={styles.flexColumnDiv}>
+              Currency
+              <span>{selectedFacility && selectedFacility.currency}</span>
             </td>
-            <td className={styles.flexColumnDiv}>Limit
-            <span>{selectedFacility && selectedFacility.limit.toFixed(2)}</span>
+            <td className={styles.flexColumnDiv}>
+              Limit
+              <span>
+                {selectedFacility && selectedFacility.limit.toFixed(2)}
+              </span>
             </td>
-            <td className={styles.flexColumnDiv}>Start Date
-            <span>{selectedFacility != null ? new Date(selectedFacility.startDate).toLocaleDateString():""}</span>
+            <td className={styles.flexColumnDiv}>
+              Start Date
+              <span>
+                {selectedFacility != null
+                  ? new Date(selectedFacility.startDate).toLocaleDateString()
+                  : ""}
+              </span>
             </td>
           </tr>
           <tr>
-            <td className={styles.flexColumnDiv}>Maturity Date
-            <span>{selectedFacility != null ? new Date(selectedFacility.maturityDate).toLocaleDateString():""}</span>
+            <td className={styles.flexColumnDiv}>
+              Maturity Date
+              <span>
+                {selectedFacility != null
+                  ? new Date(selectedFacility.maturityDate).toLocaleDateString()
+                  : ""}
+              </span>
             </td>
           </tr>
         </table>
-      ): <div>
-          No facilities found
-          </div>}
+      ) : (
+        <div>No facilities found</div>
+      )}
     </>
   );
 }
